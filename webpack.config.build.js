5df676b47f7b54f557565b6cc76fbd04522d86f0
Jonathan <jonathan.barnes@asos.com>
2018-10-11 11:11:26 +0100

    Copy change. Add public dir to ./src.

--- webpack.config.build.js
+++ webpack.config.build.js
@@ -1,6 +1,15 @@
 const CleanWebpackPlugin = require("clean-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
 
 module.exports = {
   mode: "production",
-  plugins: [new CleanWebpackPlugin(["dist"])]
+  plugins: [
+    new CleanWebpackPlugin(["dist"]),
+    new CopyWebpackPlugin([
+      {
+        from: "./src/public/**/*",
+        to: "./[name].[ext]"
+      }
+    ])
+  ]
 };
