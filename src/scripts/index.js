0d5a4dc338390f5720b0e2e57e58a41266c02a84
Jonathan <jonathan.barnes@live.co.uk>
2020-03-14 19:40:40 +0000

    add wip

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,2 +1,35 @@
 import "babel-polyfill";
 import "../styles/main.scss";
+
+const spans = document.querySelectorAll("span");
+const updates = document.querySelectorAll(".u");
+const ins = document.querySelectorAll(".i");
+const progresses = document.querySelectorAll(".p");
+
+function getRandomIndex(length) {
+  return Math.floor(Math.random() * (length - 1));
+}
+
+function getRandomElement(collection) {
+  return collection[getRandomIndex(collection.length)];
+}
+
+function start() {
+  setInterval(() => {
+    for (const span of spans) {
+      span.classList.remove("on");
+    }
+
+    const randoms = [
+      getRandomElement(updates),
+      getRandomElement(ins),
+      getRandomElement(progresses)
+    ];
+
+    for (const r of randoms) {
+      r.classList.add("on");
+    }
+  }, 1000);
+}
+
+start();
